8c21290230d33be88aca5efbfd4c31b3c6af254f
kangman53 <hermanboy53@gmail.com>
2018-11-26 13:52:54 +0700

    money coins done

--- moneyCoins.js
+++ moneyCoins.js
@@ -1,5 +1,17 @@
 function moneyCoins (money) {
   // your implementation code here
+  var coins = [1, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000];
+  var result = [];
+  while (money != 0){
+    for(var i = coins.length-1; i >= 0; i--){
+      if(money >= coins[i]){
+        result.push(coins[i]);
+        money -= coins[i];
+        i++;
+      }
+    }
+  }
+  return result;
 }
 
 // Drive code
