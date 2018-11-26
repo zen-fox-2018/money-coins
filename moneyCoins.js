0c9d9a76ed262a886635352e4c932653b8e9f08b
Arief Rachman <arievrchman@gmail.com>
2018-11-26 21:58:03 +0700

    upload money coins

--- moneyCoins.js
+++ moneyCoins.js
@@ -1,5 +1,20 @@
-function moneyCoins (money) {
+function moneyCoins(money) {
   // your implementation code here
+  const coin = [10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 1];
+
+  let convertCoin = [];
+
+  for (let i = 0; i < coin.length; i++) {
+    if (money >= coin[i]) {
+      convertCoin.push(coin[i]);
+
+      money -= coin[i];
+      i--
+    }
+  }
+
+  return convertCoin
+
 }
 
 // Drive code
